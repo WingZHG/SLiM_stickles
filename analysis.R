e79b3699ffadd3e09b3bd96237439f4927c44bcd
WingZHG <wingzhengho@hotmail.com>
2022-09-21 00:25:31 -0400

    assortative mating

--- analysis.R
+++ analysis.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 
-generations <- 5
+generations <- 10
 
 ancestry_mut <- read.csv(file = paste0(getwd(), "/output/mut.csv"))
 ancestry_true <- read.csv(file = paste0(getwd(), "/output/sticklebacks_ancestryproportions.csv"))
@@ -20,3 +20,16 @@ plot(ancestry_mut$P3AncestryProportion ~ ancestry_true$P3AncestryProportion, mai
 
 corresult <- cor.test(ancestry_mut$P4AncestryProportion , ancestry_true$P4AncestryProportion)
 plot(ancestry_mut$P4AncestryProportion ~ ancestry_true$P4AncestryProportion, main = paste("P4 proportion ", "Correlation r = " , round(corresult$estimate, 5), " in Gen ", generations))
+
+Freq_dropout <-  rowSums(1*as.matrix((ancestry_true > 0) & (ancestry_mut == 0)))
+sum(Freq_dropout>0) / length(Freq_dropout)
+
+Num_categories <- 2^generations
+fractions <- 1/Num_categories
+Actual_ancestry_bin <- round(ancestry_true*Num_categories)/Num_categories
+Inferred_ancestry_bin <- round(ancestry_mut*Num_categories)/Num_categories
+#table(Inferred_ancestry_bin$P1AncestryProportion, Actual_ancestry_bin$P1AncestryProportion)
+Nwrong_bins <- rowSums(1*as.matrix(Actual_ancestry_bin != Inferred_ancestry_bin))
+sum(Nwrong_bins > 0)
+miscat <- sum(Nwrong_bins > 0)
+miscat / 1000
