86bf0fe3241d85250feeb10a044cc8ec7bcc47fd
wingzhg <wingzhg@pop-os.localdomain>
2023-03-09 02:56:47 -0500

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 for i in {1..20}; do
 	slim -d REP="$i" "stickles - Copy.slim"
-	python "localancestry_proportions - F.py" ./output/sticklebacks_$i.trees &
+	python "localancestry_proportions - F.py" ./output/sticklebacks_$i.trees & test
 done
 
